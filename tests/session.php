1e0229e632527a619524591dfa7595534b771a5b
Антон Кияткин <aky@list.ru>
2015-08-07 13:58:43 +0400

    psr2

--- tests/session.php
+++ tests/session.php
@@ -2,20 +2,24 @@
 require_once(__DIR__.'/../session.php');
 $ans=array();
 $ans['title']='Проверка сессии на сервере';
-
+if (!class_exists('PDO')) {
+	return infra_err($ans, 'class PDO is required');
+}
 $db=&infra_db();
-if(!$db)return infra_err($ans,'ERROR нет базы данных');
+if (!$db) {
+	return infra_err($ans, 'ERROR нет базы данных');
+}
 $val=infra_session_get('test');
 
 
 
 $val=(int)$val+1;
-infra_session_set('test',$val);
+infra_session_set('test', $val);
 
 $d=infra_session_get();
 $ans['test']=$d['test'];
-if($d['test']>1){
-	return infra_ret($ans,'PASS');
-}else{
-	return infra_err($ans,'ERROR нажмите 1 раз F5');
+if ($d['test']>1) {
+	return infra_ret($ans, 'PASS');
+} else {
+	return infra_err($ans, 'ERROR нажмите 1 раз F5');
 }
