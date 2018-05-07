d4028eadc1f8f8798230bd1d307d62844729f21b
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2018-05-07 19:17:20 +0400

    fix login.php

--- login.php
+++ login.php
@@ -2,31 +2,16 @@
 use infrajs\session\Session;
 use infrajs\path\Path;
 use infrajs\view\View;
-use infrajs\config\Config;
-use infrajs\infra\Infra;
-
-
-if (!is_file('vendor/autoload.php')) {
-	chdir(explode('vendor/', __DIR__)[0]);
-	require_once('vendor/autoload.php');
-}
+use infrajs\ans\Ans;
 
 $ans = array();
-$id = $_REQUEST['id'];
-$pass = $_REQUEST['pass'];//md5 пароля, чтобы авторизоваться не нужно знать пароль, хэша достаточно.
-$src = $_REQUEST['src'];
+$id = Ans::REQ('id','int');
+$pass = Ans::REQ('pass');//md5 пароля, чтобы авторизоваться не нужно знать пароль, хэша достаточно.
+$src = Ans::REQ('src','string','');
 
 if ($id && $pass) {
 	Session::change($id, $pass);
 }
 
-if (!$src) {
-	$src = '';
-} else {
-	$src = '/'.$src;
-}
-$conf = Config::get();
-$path = 'http://'.View::getHost().'/';
-
-$path .= View::getRoot().$src;
+$path = View::getPath().$src;
 @header('Location: '.$path);
